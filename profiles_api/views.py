c393e64ae7ded09322a88b21605e978690326975
Chipz <chipz.ciptadi@live.com>
2019-09-15 16:53:50 +0700

    added viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 
 from profiles_api import serializers
 
@@ -31,7 +32,10 @@ class HelloApiView(APIView):
             name = serializer.validated_data.get('name')
             message = f"Hello, {name}"
 
-            return Response({"message": message})
+            return Response(
+                {"message": message},
+                status=status.HTTP_201_CREATED,
+            )
 
         else:
             return Response(
@@ -53,3 +57,56 @@ class HelloApiView(APIView):
         """Deletes and object"""
         
         return Response({"method": "DELETE"})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+
+    serializer_class = serializers.HelloSerializer
+
+    def list(self, request):
+        """Return a hello message"""
+        a_viewset = [
+            "Uses actions (list, create, retrieve, update, partial_update)",
+            "Automatically maps to URLs using Routers",
+            "Provides more funcitonality with less code",
+        ]
+
+        return Response({
+            "message": "Hello",
+            "a_viewset": a_viewset
+        })
+
+    def create(self, request):
+        """Create a new hello-viewset message"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get("name")
+            message = f"Hello, {name}!"
+
+            return Response(
+                {"message": message}, 
+                status.HTTP_201_CREATED
+            )
+        else:
+            return Response(
+                serializer.errors, 
+                status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrieve(self, request, pk=None):
+        """Return object by its ID"""
+        return Response({"Method": "GET"})
+    
+    def update(self, request, pk=None):
+        """Update object"""
+        return Response({"Method": "PUT"})
+
+    def partial_update(self, request, pk=None):
+        """Update part of an object"""
+        return Response({"Method": "PATCH"})
+
+    def destroy(self, request, pk=None):
+        """Delete an object"""
+        return Response({"Method": "DELETE"})
